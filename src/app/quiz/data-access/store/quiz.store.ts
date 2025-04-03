102e3f23bf0d339690ed9b5909cf1ee9daf12f37
joseinyork <joseinyork@googlemail.com>
2025-04-03 21:20:26 +0000

    feat(quiz): add store methods

--- src/app/quiz/data-access/store/quiz.store.ts
+++ src/app/quiz/data-access/store/quiz.store.ts
@@ -1,12 +1,48 @@
-import { signalStore, withState } from '@ngrx/signals';
+import { computed, inject } from '@angular/core';
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
 import { QuizState } from '@quiz/data-access/quiz.models';
+import { QuizService } from '../services/quiz.service';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { debounceTime, pipe, switchMap, tap } from 'rxjs';
 
 
 
 const initialState: QuizState = {
   questions:[],
-  activeQuestion: -1
+  activeQuestion: 0,
+  answers:[]
 };
 
-export const QuizStore = signalStore(withState(initialState));
+export const QuizStore = signalStore(withState(initialState),
+withMethods((store, quizService = inject(QuizService))=>({
+  nextQuestion:()=> {
+    patchState(store, (state)=>({ activeQuestion:store.activeQuestion()+1}))
+  },
+  prevQuestion:()=> {
+    patchState(store, (state)=>({ activeQuestion:store.activeQuestion()-1}))
+  },
+  loadQuestions:rxMethod<void>(
+    pipe(
+      debounceTime(300),
+      switchMap(()=> quizService.getQuestions().pipe(
+        tap((data)=>{
+          patchState(store, ({questions})=>({questions:[...questions, ...data]}))
+        }
+      ))
+    )
+  )
+})),
+withComputed((store)=>({
+  score:computed(()=> {
+    let score = 0;
+
+    store.answers().forEach((answer, index)=>{
+      if(store.questions()[index].correctAnswer===answer){
+        score ++;
+      }
+
+    })
+    return score;
+  } )
+})));
 
