2c33e22a4a24876c7565494107fe3f0ba353c042
@diazjdev <joseinyork+git@gmail.com>
2025-05-04 16:34:45 +0100

    feat(score): implement quiz score

--- src/app/quiz/feature/score/score.component.ts
+++ src/app/quiz/feature/score/score.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, computed, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { QuizFacade } from '@quiz/data-access/facades/quiz.facade';
 
 @Component({
   selector: 'app-score',
@@ -10,23 +11,23 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class ScoreComponent {
   #router = inject(Router);
-  // #quizStore = inject(QuizStore);
+  #quizFacade = inject(QuizFacade);
+  score = this.#quizFacade.quizScore;
 
-  // score = this.#quizStore.score;
   scoreMessage = computed(() => {
-    // if (this.score() >= 0 && this.score() < 4) {
-    //   return 'You may want to do the quiz again...';
-    // }
+    if (this.score() >= 0 && this.score() < 4) {
+      return 'You may want to do the quiz again...';
+    }
 
-    // if (this.score() >= 4 && this.score() < 8) {
-    //   return 'That was a very good effort!';
-    // }
+    if (this.score() >= 4 && this.score() < 8) {
+      return 'That was a very good effort!';
+    }
 
     return 'Congratulations! Perfect score';
   });
 
   startQuizAgain() {
-    // this.#quizStore.resetQuiz();
+    this.#quizFacade.resetQuiz();
     this.#router.navigate(['quiz']);
   }
 }
