b88e7da99960774836da8599057d510358201bab
@diazjdev <joseinyork+git@gmail.com>
2025-05-04 14:04:39 +0100

    feat(quiz): question form

--- src/app/quiz/ui/question/question.component.ts
+++ src/app/quiz/ui/question/question.component.ts
@@ -1,20 +1,36 @@
-import { Component, EventEmitter, input, Output, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, input, OnInit, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Question } from '@quiz/data-access/quiz.models';
 
 @Component({
   selector: 'app-question',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './question.component.html',
-  styleUrl: './question.component.css'
+  styleUrl: './question.component.css',
 })
-export class QuestionComponent {
-  question = input<Question | null>(null);
-  selectedAnswer = input<number>(-1);
+export class QuestionComponent implements OnInit {
   @Output() answerQuestion = new EventEmitter<number>();
 
+  question = input<Question | null>(null);
+  selectedAnswer = input<number>(-1);
 
-  onAnswerQuestion(answerIndex:number){
-    this.answerQuestion.emit(answerIndex);
+  questionForm = new FormGroup({
+    question: new FormControl<number>(-1),
+  });
+
+  ngOnInit() {
+    this.questionForm.valueChanges.subscribe(({ question }) => {
+      this.onAnswerQuestion(question!);
+    });
   }
 
+  onAnswerQuestion(answerIndex: number) {
+    this.answerQuestion.emit(answerIndex);
+  }
 }
